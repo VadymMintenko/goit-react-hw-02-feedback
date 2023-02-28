105f5566a2e95a68f3b143d1600d3bfe3e04256c
VadymMintenko <vad.m1ntenko@gmail.com>
2023-02-28 23:55:21 +0200

    commit

--- src/components/Container.styled.js
+++ src/components/Container.styled.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   text-align: center;
-  padding: 20px;
+  padding: 21px;
   border: solid 1px gray;
   border-radius: 5px;
   background: rgb(34, 193, 195);
